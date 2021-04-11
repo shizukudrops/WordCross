3ad8d6e65c00fb3d83064616903ddc4b1eacd469
Hidenobu Yamada <hyamada0307@gmail.com>
2021-04-11 09:35:36 +0900

    Basic Function

---
+++ WordCross/DictionaryInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCross
+{
+    public class DictionaryInfo
+    {
+        public string Name { get; set; }
+        public string BaseUrl { get; set; }
+        public string Separator { get; set; }
+
+        public DictionaryInfo(string name, string baseUrl) 
+        {
+            Name = name;
+            BaseUrl = baseUrl;
+        }
+
+        public DictionaryInfo(string name, string baseUrl, string separator)
+        {
+            Name = name;
+            BaseUrl = baseUrl;
+            Separator = separator;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- WordCross/MainPage.xaml.cs
+++ WordCross/MainPage.xaml.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -22,18 +28,84 @@ namespace WordCross
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        readonly ObservableCollection<DictionaryInfo> dictView = new ObservableCollection<DictionaryInfo>();
+
+        object RightClickedItem;
+
         public MainPage()
         {
             this.InitializeComponent();
+            
+            dictView.Add(new DictionaryInfo("Longman", "https://www.ldoceonline.com/jp/dictionary/", "-"));
+            dictView.Add(new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-"));
+            dictView.Add(new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20"));
+            dictView.Add(new DictionaryInfo("英辞郎", "https://eow.alc.co.jp/", "+"));
+            dictView.Add(new DictionaryInfo("DictJuggler", "https://www.dictjuggler.net/yakugo/?word=", "%20"));
+
+            dictList.ItemsSource = dictView;
         }
 
-        private void OnKeyDownHandler(object sender, KeyRoutedEventArgs e)
+        public void AddNewDictionary(DictionaryInfo dict)
+        {
+            this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.dictView.Add(dict));
+        }
+
+        private void dictList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            search((DictionaryInfo)dictList.SelectedItem, searchBox.Text);
+        }
+
+        private void searchBox_KeyDown(object sender, KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Enter)
             {
-                var target = "https://www.ldoceonline.com/jp/dictionary/" + searchBox.Text;
-                webView1.Source = new Uri(target);
+                search((DictionaryInfo)dictList.SelectedItem, searchBox.Text);
             }
+        }
+
+        private void search(DictionaryInfo dict, string input)
+        {
+            //どの辞書も選ばれていなかったら一番上の辞書で検索する。辞書が存在しなければ戻る。
+            if (dict == null)
+            {
+                if (dictList.Items.Count > 0) dict = (DictionaryInfo)dictList.Items.First();
+                else return;
+            }
+
+            var words = input.Split(" ");
+            var target = string.Join(dict.Separator, words);
+            var final = dict.BaseUrl + target;
+            webView1.Source = new Uri(final);
+        }
+
+        private void dictList_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            ListView listView = (ListView)sender;
+            DictListContextMenu.ShowAt(listView, e.GetPosition(listView));
+            RightClickedItem = ((FrameworkElement)e.OriginalSource).DataContext;
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            dictView.Remove((DictionaryInfo)RightClickedItem);
+        }
+
+        async private void AddDictionary_Click(object sender, RoutedEventArgs e)
+        {
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(AddDictionary), this);
+                Window.Current.Content = frame;
+
+                // You have to activate the window in order to show it later.
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
         }
     }
 }
