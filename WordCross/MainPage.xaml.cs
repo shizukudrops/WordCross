0ba35f1bfddfdfd5f8e16bec321e7167fe3bfd86
Hidenobu Yamada <hyamada0307@gmail.com>
2021-04-20 03:50:53 +0900

    Switch default dictionaries by system language

--- WordCross/MainPage.xaml.cs
+++ WordCross/MainPage.xaml.cs
@@ -54,6 +54,9 @@ namespace WordCross
         {
             this.InitializeComponent();
 
+            //環境の言語を取得
+            var language = ApplicationLanguages.Languages.First();
+
             //広告ブロック機能のオンオフをトグルに反映
             disableAdsToggle.IsChecked = IsAdsDisabled;
 
@@ -62,19 +65,35 @@ namespace WordCross
 
             if (dictionariesString == null)
             {
+                //辞書リストが存在しない場合（初回起動時）
+
                 dictView = new ObservableCollection<DictionaryInfo>();
-                dictView.Add(new DictionaryInfo("Longman", "https://www.ldoceonline.com/jp/dictionary/", "-"));
-                dictView.Add(new DictionaryInfo("Oxford Learners", "https://www.oxfordlearnersdictionaries.com/definition/english/", "-"));
-                dictView.Add(new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-"));
-                dictView.Add(new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20"));
-                dictView.Add(new DictionaryInfo("英辞郎", "https://eow.alc.co.jp/", "+"));
-                dictView.Add(new DictionaryInfo("Weblio英和・和英", "https://ejje.weblio.jp/content/", "+"));
-                dictView.Add(new DictionaryInfo("goo辞書英和・和英", "https://dictionary.goo.ne.jp/word/en/", "+"));
-                dictView.Add(new DictionaryInfo("DictJuggler", "https://www.dictjuggler.net/yakugo/?word=", "%20"));
-                dictView.Add(new DictionaryInfo("WordNet 3.1", "http://wordnetweb.princeton.edu/perl/webwn?s=", "+"));
-                dictView.Add(new DictionaryInfo("Wikipedia日本語版", "https://ja.wikipedia.org/wiki/", ""));
-                dictView.Add(new DictionaryInfo("Wikipedia English", "https://en.wikipedia.org/wiki/", ""));
-                dictView.Add(new DictionaryInfo("goo辞書国語", "https://dictionary.goo.ne.jp/word/", "+"));
+
+                if (language == "ja-JP" || language == "ja")
+                {
+                    dictView.Add(new DictionaryInfo("Longman", "https://www.ldoceonline.com/jp/dictionary/", "-"));
+                    dictView.Add(new DictionaryInfo("Oxford Learners", "https://www.oxfordlearnersdictionaries.com/definition/english/", "-"));
+                    dictView.Add(new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-"));
+                    dictView.Add(new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20"));
+                    dictView.Add(new DictionaryInfo("英辞郎", "https://eow.alc.co.jp/", "+"));
+                    dictView.Add(new DictionaryInfo("Weblio英和・和英", "https://ejje.weblio.jp/content/", "+"));
+                    dictView.Add(new DictionaryInfo("goo辞書英和・和英", "https://dictionary.goo.ne.jp/word/en/", "+"));
+                    dictView.Add(new DictionaryInfo("DictJuggler", "https://www.dictjuggler.net/yakugo/?word=", "%20"));
+                    dictView.Add(new DictionaryInfo("WordNet 3.1", "http://wordnetweb.princeton.edu/perl/webwn?s=", "+"));
+                    dictView.Add(new DictionaryInfo("Wikipedia日本語版", "https://ja.wikipedia.org/wiki/", ""));
+                    dictView.Add(new DictionaryInfo("Wikipedia English", "https://en.wikipedia.org/wiki/", ""));
+                    dictView.Add(new DictionaryInfo("goo辞書国語", "https://dictionary.goo.ne.jp/word/", "+"));
+                }
+                else
+                {
+                    dictView.Add(new DictionaryInfo("Longman", "https://www.ldoceonline.com/jp/dictionary/", "-"));
+                    dictView.Add(new DictionaryInfo("Oxford Learners", "https://www.oxfordlearnersdictionaries.com/definition/english/", "-"));
+                    dictView.Add(new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-"));
+                    dictView.Add(new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20"));
+                    dictView.Add(new DictionaryInfo("WordNet 3.1", "http://wordnetweb.princeton.edu/perl/webwn?s=", "+"));
+                    dictView.Add(new DictionaryInfo("Wikipedia English", "https://en.wikipedia.org/wiki/", ""));
+                }
+                
             }
             else
             {
@@ -84,8 +103,6 @@ namespace WordCross
             dictList.ItemsSource = dictView;
 
             //スタートページの表示と言語による切り替え
-            var language = ApplicationLanguages.Languages.First();
-
             if (language == "ja-JP" || language == "ja")
             {
                 webView.Navigate(new Uri("ms-appx-web:///Assets/startpage_ja.html"));
