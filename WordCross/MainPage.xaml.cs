a08e8cf83a38c0214bc72701a46b8d8108f2b9b1
Hidenobu Yamada <hyamada0307@gmail.com>
2021-04-20 03:36:51 +0900

    Add default dictionaries

--- WordCross/MainPage.xaml.cs
+++ WordCross/MainPage.xaml.cs
@@ -68,12 +68,13 @@ namespace WordCross
                 dictView.Add(new DictionaryInfo("Collins Thesaurus", "https://www.collinsdictionary.com/dictionary/english-thesaurus/", "-"));
                 dictView.Add(new DictionaryInfo("Merriam Webster", "https://www.merriam-webster.com/dictionary/", "%20"));
                 dictView.Add(new DictionaryInfo("英辞郎", "https://eow.alc.co.jp/", "+"));
-                dictView.Add(new DictionaryInfo("goo辞書英和", "https://dictionary.goo.ne.jp/word/en/", "+"));
-                dictView.Add(new DictionaryInfo("Weblio", "https://ejje.weblio.jp/content/", "+"));
+                dictView.Add(new DictionaryInfo("Weblio英和・和英", "https://ejje.weblio.jp/content/", "+"));
+                dictView.Add(new DictionaryInfo("goo辞書英和・和英", "https://dictionary.goo.ne.jp/word/en/", "+"));
                 dictView.Add(new DictionaryInfo("DictJuggler", "https://www.dictjuggler.net/yakugo/?word=", "%20"));
                 dictView.Add(new DictionaryInfo("WordNet 3.1", "http://wordnetweb.princeton.edu/perl/webwn?s=", "+"));
                 dictView.Add(new DictionaryInfo("Wikipedia日本語版", "https://ja.wikipedia.org/wiki/", ""));
                 dictView.Add(new DictionaryInfo("Wikipedia English", "https://en.wikipedia.org/wiki/", ""));
+                dictView.Add(new DictionaryInfo("goo辞書国語", "https://dictionary.goo.ne.jp/word/", "+"));
             }
             else
             {
